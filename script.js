9bf14c92a479066422bb056f7b894dff922a39be
Facaller <ashwin.metal13@gmail.com>
2024-01-24 21:27:19 +0200

    Neaten grid & add hover effect to tiles

--- script.js
+++ script.js
@@ -2,17 +2,32 @@ const container = document.createElement('div');
 document.body.appendChild(container);
 container.classList.add("container")
 
-function makeRows (rowNum, columnNum) {
+function makeGrid (rowNum, columnNum) {
     for (r = 0; r < rowNum; r++) {
         for (c = 1; c < columnNum; c++) {
             let column = document.createElement('div')
             container.appendChild(column);
             column.classList.add("column")
+
+            column.addEventListener('mouseover', () => {
+                column.style.backgroundColor = 'black';
+            });
+            column.addEventListener('mouseout', () => {
+                column.style.backgroundColor = '';
+            });
         }
+        
         let row = document.createElement('div');
         container.appendChild(row);
         row.classList.add("row")
+
+        row.addEventListener('mouseover', () => {
+            row.style.backgroundColor = 'black';
+        });
+        row.addEventListener('mouseout', () => {
+            row.style.backgroundColor = '';
+        });
     }
 }
 
-makeRows(16, 16)
+makeGrid(16, 16)
